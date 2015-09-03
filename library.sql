121cdc23d597f71ce3add566c04668c01f10a152
colleen minor & haley harrison <student@epicodus.com>
2015-09-03 10:51:35 -0700

    Improved library.sql

--- library.sql
+++ library.sql
@@ -130,6 +130,108 @@ ALTER SEQUENCE books_id_seq OWNED BY books.id;
 
 
 --
+-- Name: checkouts; Type: TABLE; Schema: public; Owner: Guest; Tablespace:
+--
+
+CREATE TABLE checkouts (
+    id integer NOT NULL,
+    patron_id integer,
+    book_id integer,
+    due_date character varying
+);
+
+
+ALTER TABLE checkouts OWNER TO "Guest";
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE checkouts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE checkouts_id_seq OWNER TO "Guest";
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE checkouts_id_seq OWNED BY checkouts.id;
+
+
+--
+-- Name: copies; Type: TABLE; Schema: public; Owner: Guest; Tablespace:
+--
+
+CREATE TABLE copies (
+    id integer NOT NULL,
+    book_id integer,
+    num_copies integer
+);
+
+
+ALTER TABLE copies OWNER TO "Guest";
+
+--
+-- Name: copies_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE copies_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE copies_id_seq OWNER TO "Guest";
+
+--
+-- Name: copies_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE copies_id_seq OWNED BY copies.id;
+
+
+--
+-- Name: patrons; Type: TABLE; Schema: public; Owner: Guest; Tablespace:
+--
+
+CREATE TABLE patrons (
+    id integer NOT NULL,
+    patron_name character varying
+);
+
+
+ALTER TABLE patrons OWNER TO "Guest";
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE patrons_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE patrons_id_seq OWNER TO "Guest";
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE patrons_id_seq OWNED BY patrons.id;
+
+
+--
 -- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
 --
 
@@ -148,6 +250,27 @@ ALTER TABLE ONLY authors_books ALTER COLUMN id SET DEFAULT nextval('authors_book
 --
 
 ALTER TABLE ONLY books ALTER COLUMN id SET DEFAULT nextval('books_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY checkouts ALTER COLUMN id SET DEFAULT nextval('checkouts_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY copies ALTER COLUMN id SET DEFAULT nextval('copies_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY patrons ALTER COLUMN id SET DEFAULT nextval('patrons_id_seq'::regclass);
 
 
 --
@@ -200,6 +323,51 @@ SELECT pg_catalog.setval('books_id_seq', 8, true);
 
 
 --
+-- Data for Name: checkouts; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY checkouts (id, patron_id, book_id, due_date) FROM stdin;
+\.
+
+
+--
+-- Name: checkouts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('checkouts_id_seq', 1, false);
+
+
+--
+-- Data for Name: copies; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY copies (id, book_id, num_copies) FROM stdin;
+\.
+
+
+--
+-- Name: copies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('copies_id_seq', 1, false);
+
+
+--
+-- Data for Name: patrons; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY patrons (id, patron_name) FROM stdin;
+\.
+
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('patrons_id_seq', 1, false);
+
+
+--
 -- Name: authors_books_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace:
 --
 
@@ -221,6 +389,30 @@ ALTER TABLE ONLY authors
 
 ALTER TABLE ONLY books
     ADD CONSTRAINT books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: checkouts_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace:
+--
+
+ALTER TABLE ONLY checkouts
+    ADD CONSTRAINT checkouts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: copies_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace:
+--
+
+ALTER TABLE ONLY copies
+    ADD CONSTRAINT copies_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: patrons_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace:
+--
+
+ALTER TABLE ONLY patrons
+    ADD CONSTRAINT patrons_pkey PRIMARY KEY (id);
 
 
 --
